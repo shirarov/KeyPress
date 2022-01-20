bfa12e15525ab15dd498948ce078ffc536bb6faf
Shira Rubanowitz <shira.rubanowitz@tandemg.com>
2022-01-20 15:49:50 +0200

    first commit

---
+++ targilDotNet/targilDotNet/Migrations/20220119082723_InitialCreate.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace targilDotNet.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Keys",
+                columns: table => new
+                {
+                    KeyName = table.Column<string>(type: "nvarchar(1)", nullable: false),
+                    startTimeastamp = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Count = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Keys", x => new { x.KeyName, x.startTimeastamp });
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Keys");
+        }
+    }
+}
---
+++ targilDotNet/targilDotNet/Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Microsoft.Extensions.Configuration;
+
+namespace targilDotNet
+{
+    class Program
+    {
+        
+        
+        
+        
+        private static KeyPressContext context=new KeyPressContext();
+        static void Main(string[] args)
+        {
+            context.Database.EnsureCreated();
+               var Configuration = new ConfigurationBuilder()
+
+              .AddJsonFile("appsettings.json", false)
+                  .Build();
+            Console.WriteLine("let's start!!");
+            //instance of class CountKeyPress
+            var CountKeyPress =new CountKeyPress(Configuration);
+        }
+
+    }
+}
