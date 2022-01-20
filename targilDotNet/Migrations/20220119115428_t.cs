aa81bdd3a1905f1e55089ff86dda13b63a3141c7
shirarov <98096120+shirarov@users.noreply.github.com>
2022-01-20 14:58:38 +0200

    Add files via upload

---
+++ targilDotNet/Migrations/20220119115428_t.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace targilDotNet.Migrations
+{
+    public partial class t : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "girlses",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    type = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_girlses", x => x.id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "girlses");
+        }
+    }
+}
---
+++ targilDotNet/girl.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace targilDotNet
+{
+    public class girl
+    {
+        [Key]
+        public int id { get; set; }
+        public string name { get; set; }
+        public string type { get; set; }
+
+    }
+}
